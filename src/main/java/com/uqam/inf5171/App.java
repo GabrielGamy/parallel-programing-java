3dff4b039b292d7b8839a821f84384a1003e63b9
Gabriel Y Gamy <gabrielyakpaoro@gmail.com>
2016-12-03 17:32:31 -0500

    get the distance and duration between two postal code

--- src/main/java/com/uqam/inf5171/App.java
+++ src/main/java/com/uqam/inf5171/App.java
@@ -2,12 +2,24 @@ package com.uqam.inf5171;
 
 import com.uqam.inf5171.google.api.GoogleApi;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class App 
 {
     public static void main( String[] args )
     {
         GoogleApi api = new GoogleApi();
         
-        api.getDistance("H8R2Y7", "H8N 3E4");  
+        JSONObject result = api.getDistance("H8R2Y7", "H8N 3E4"); 
+
+        System.out.println("statusCode: " + result.get("statusCode"));
+        System.out.println("message: " + result.get("message"));
+        System.out.println("search: " + result.get("searchInfo"));
+        System.out.println("error: " + result.get("error"));
+
+        if((Integer) result.get("statusCode") == 200) {
+            System.out.println("duration: " + result.get("duration"));
+        }
     }
 }
--- src/main/java/com/uqam/inf5171/google.api/GoogleApi.java
+++ src/main/java/com/uqam/inf5171/google.api/GoogleApi.java
@@ -7,43 +7,70 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.UnknownHostException;
-import java.io.StringReader;
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.json.JsonValue;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
 
 public class GoogleApi {
 
-    private final String key = "AIzaSyCmzFiJqWHGY66Gu8acevWwsR2Hq--hh4U";
+    private final String KEY = "AIzaSyCmzFiJqWHGY66Gu8acevWwsR2Hq--hh4U";
 
     public GoogleApi(){}
     
-    public void getDistance(String originPostalCode, String destinationPostalCode) {
+    public JSONObject getDistance(String originPostalCode, String destinationPostalCode) {
+
+        JSONObject response = new JSONObject();
+        String searchInfo = "";
+        String errorMsg = "";
+
         try{
             originPostalCode = originPostalCode.replaceAll("\\s+","");
             destinationPostalCode = destinationPostalCode.replaceAll("\\s+","");
+            searchInfo = "Origin (" + originPostalCode + ") --­> Destination (" + destinationPostalCode + ")";
 
             String url = "https://maps.googleapis.com/maps/api/directions/json";
-            url += "?origin=" + originPostalCode + "&destination=" + destinationPostalCode + "&key=" + key;
+            url += "?origin=" + originPostalCode + "&destination=" + destinationPostalCode + "&key=" + KEY;
 
             URL obj = new URL(url);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
             con.setRequestMethod("GET");
 
-            int responseCode = con.getResponseCode();
-            System.out.println("Response Code : " + responseCode);
+            String dataReceived = convertResponseToString(con.getInputStream());
 
-            String response = convertResponseToString(con.getInputStream());
-            //System.out.println(response);        
+            response = responseAsJson(dataReceived);
+
         }catch(UnknownHostException ex){
-            System.out.println("Error: Check your internet connection and try again ...");
+            errorMsg = "Check your internet connection and try again: " + searchInfo;
         }catch(Exception ex){
-            System.out.println("Error: An incident occurred. Please try again !");
+            errorMsg = "An incident occurred: " + searchInfo;
+        }finally{
+            boolean success = (response != null) && !response.isEmpty();
+            return sendResponse(response, searchInfo, errorMsg, success);
+        } 
+    }
+
+    public JSONObject responseAsJson(String response) throws IOException {
+        JSONObject responseToSend = new JSONObject();
+        JSONParser parser = new JSONParser();
+
+        try{
+            JSONObject dataReceived = (JSONObject) parser.parse(response);
+
+            JSONArray routes = (JSONArray) dataReceived.get("routes");
+            
+            JSONObject routesObj = (JSONObject) routes.get(0);
+            
+            JSONArray legs = (JSONArray) routesObj.get("legs");
+
+            responseToSend = (JSONObject) legs.get(0);
+        }catch(Exception ex){
+
+        }finally{
+            return responseToSend;
         }
-          
     }
 
     private String convertResponseToString(InputStream is) throws IOException {
@@ -59,4 +86,20 @@ public class GoogleApi {
 
         return response.toString();
     }
+
+    private JSONObject sendResponse(JSONObject response, String searchInfo, 
+                                    String errorMsg, boolean success){
+                                        
+        int status = success ? 200 : 404;
+        String message = success ? "Successful request": "Failed request";
+
+        if(response == null) response = new JSONObject();
+        
+        response.put("statusCode", status);
+        response.put("message", message);
+        response.put("error", errorMsg);
+        response.put("searchInfo", searchInfo);  
+
+        return response;      
+    }        
 }
